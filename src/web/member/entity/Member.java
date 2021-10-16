1010caa939b5fcf5bf22c08fc0e2301589423f4f
William <ithan0117@gmail.com>
2021-10-16 22:40:07 +0800

    Member - add entity class

---
+++ src/web/member/entity/Member.java
@@ -0,0 +1,12 @@
+package web.member.entity;
+
+import java.sql.Timestamp;
+
+public class Member {
+	public Integer id;
+	public String username;
+	public String password;
+	public String nickname;
+	public Boolean pass;
+	public Timestamp createTime;
+}
